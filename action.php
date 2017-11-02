2317787de48dfc9fab3c0b70a7836573fb8925b4
chananate khamperngjai <chananate.k@gmail.com>
2017-11-03 01:37:58 +0700

    whatever

--- action.php
+++ action.php
@@ -1,11 +1,20 @@
 <?php include "include/header.php"; ?>
+<?php
+    include('include/db.php');
+
+    $db = new DB();
+
+    $rows = $db->Query('SELECT product.*,lib_category.name AS category_name FROM product LEFT JOIN lib_category ON product.category=lib_category.code');
+    
+?>
+<br>
+<<br>
+<<br>
 <div class="row">
 
 	<?php
-$stmt = $pdo->prepare("SELECT product.*,lib_category.name AS category_name FROM product LEFT JOIN lib_category ON product.category=lib_category.code");
-$stmt->execute();
-while ($row = $stmt->fetch()) { ?>
-	<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12" style="border-width: 1px">
+foreach($rows as $row){ ?>
+	<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12" style="border-width: 1px; color:white;">
 		<?=$row['pid'];?>
 		<br><?=$row['pname'];?>
 		<br><?=$row['category_name'];?>
@@ -14,4 +23,11 @@ while ($row = $stmt->fetch()) { ?>
 <?php }
 ?>
 
+<div class="bg" style="height:400px">
+	
+</div>
 <?php include "include/footer.php"; ?>
+
+
+
+
--- include/header.php
+++ include/header.php
@@ -1,5 +1,35 @@
 <?php include "connect.php"; ?>
-<html><head><meta charset="utf-8">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>Action Games</title>
+	<link rel="icon" type="images/png" href="logo-square.png">
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+
+	<!--[if lt IE 9] >
+   <script scr="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+   <script scr="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script> 
+  <![endif] -->
 </head>
-<body>
+
+<body class="bg">
+<div class="head1 white">
+<span class="sort">
+	<a href="#action" style="color: white">ACTION</a>
+	<a href="#adventure" style="color: white">ADVENTURE</a>
+	<a href="#strategy" style="color: white">STRATEGY</a>
+	<a href="#rpg" style="color: white">ROLE PLAYING GAME/RPG</a>
+	<a href="#simulation" style="color: white">SIMULATION</a>
+	<a href="#sports" style="color: white">SPORTS</a>
+	<a href="#racing" style="color: white">RACING</a></span>
+</div>
+
+<div >
+<a href="test.html"><img class="lo" src="images/logo-square.png" width="80px" alt=""></a>
+</div>
